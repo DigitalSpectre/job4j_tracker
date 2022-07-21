8cea0f4afe1057c1b8de6ef54761e47380fe7cfa
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2022-07-21 22:19:29 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -5,7 +5,6 @@ public class DummyDic {
         return "Неизвестное слово. " + eng;
     }
 
-
     public static void main(String[] args) {
         DummyDic dummy = new DummyDic();
         String word = dummy.engToRus("poop");

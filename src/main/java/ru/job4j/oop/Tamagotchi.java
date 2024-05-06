67227f95db8ea0b4de6821869eb2e6679cc7033e
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2024-05-07 00:50:39 +0300

    Переменные и ссылочные типы данных [#505100]

--- src/main/java/ru/job4j/oop/Tamagotchi.java
+++ src/main/java/ru/job4j/oop/Tamagotchi.java
@@ -3,6 +3,12 @@ package ru.job4j.oop;
 public class Tamagotchi {
     public static void main(String[] args) {
         int weight = 100;
-        System.out.println(weight);
+        int weightcopy = weight;
+        System.out.println("weight: " + weight);
+        System.out.println("copy of weight: " + weightcopy);
+        weightcopy += 10;
+        System.out.println("weight: " + weight);
+        System.out.println("copy of weight: " + weightcopy);
+
     }
 }

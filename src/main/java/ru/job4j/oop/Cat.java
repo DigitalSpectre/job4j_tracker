667e7d13da4a8fd84a2bedd0bf797e5d4383fa25
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2024-12-03 17:02:48 +0300

    1.5. Поля объекта [#166083 #324666]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -3,12 +3,28 @@ package ru.job4j.oop;
 public class Cat {
 
     private String food;
+    private String name;
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
 
     public void show() {
-        System.out.println(this.food);
+        System.out.println(name + " eats " + food);
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("cutlet");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }

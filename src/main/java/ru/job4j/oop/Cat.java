cb0f8a081a2513fb2c63c3d37f4243f7e90b2e39
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2024-12-03 16:50:07 +0300

    1.5. Поля объекта [#166083 #324666]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,15 +1,14 @@
 package ru.job4j.oop;
 
 public class Cat {
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+
+    private String food;
+
+    public void show() {
+        System.out.println(this.food);
     }
 
     public static void main(String[] args) {
         Cat peppy = new Cat();
-        Cat sparky = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says: " + say);
     }
 }

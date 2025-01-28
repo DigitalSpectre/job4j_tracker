5a496388313477680acb7092c867213ae0aa98f3
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2025-01-28 16:26:43 +0300

    4. Переопределение метода [#174890]

---
+++ src/main/java/ru/job4j/inheritance/Cat1.java
@@ -0,0 +1,7 @@
+package ru.job4j.inheritance;
+
+public class Cat1 {
+    public boolean canPurr() {
+        return true;
+    }
+}
--- src/main/java/ru/job4j/inheritance/ToyCat.java
+++ src/main/java/ru/job4j/inheritance/ToyCat.java
@@ -1,12 +1,12 @@
 package ru.job4j.inheritance;
 
-public class ToyCat extends Cat {
+public class ToyCat extends Cat1 {
     public boolean canBeWashByWashMachine() {
         return true;
     }
 
     public static void main(String[] args) {
-        Cat cat = new Cat();
+        Cat1 cat = new Cat1();
         System.out.println(cat.canPurr());
         ToyCat toy = new ToyCat();
         System.out.println(toy.canPurr());

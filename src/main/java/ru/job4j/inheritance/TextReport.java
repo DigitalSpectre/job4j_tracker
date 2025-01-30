86f8ee955c1d91b72424d7b87a92bc581b079e58
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2025-01-30 22:30:13 +0300

    7. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/TextReport.java
+++ src/main/java/ru/job4j/inheritance/TextReport.java
@@ -1,8 +1,8 @@
 package ru.job4j.inheritance;
 
 public class TextReport {
-
-    @Override
+/**
+    @Override */
     public String generate(String name, String body) {
         return name + System.lineSeparator() + body;
     }

06b227ed9e04ae9404c3e7189889202c61991c32
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2025-02-12 17:25:49 +0300

    14. Экранирование символов. Управляющие последовательности. [#504916]

--- src/main/java/ru/job4j/EscapeExample.java
+++ src/main/java/ru/job4j/EscapeExample.java
@@ -2,6 +2,8 @@ package ru.job4j;
 
 public class EscapeExample {
     public static void main(String[] args) {
-        System.out.print("Первая строка\nВторая строка");
+        System.out.println("Без отступа");
+        System.out.println("\tОдин отступ");
+        System.out.println("\t\tДва отступа");
     }
 }

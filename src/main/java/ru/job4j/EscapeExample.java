d6ad440f17dcc77cc55c2f9da0163483473d7032
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2025-02-12 17:22:59 +0300

    14. Экранирование символов. Управляющие последовательности. [#504916]

--- src/main/java/ru/job4j/EscapeExample.java
+++ src/main/java/ru/job4j/EscapeExample.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class EscapeExample {
     public static void main(String[] args) {
-        char escape = '\"';
-        System.out.println(escape);
+        System.out.print("Старый длинный текст\r");
+        System.out.println("Новый текст");
     }
 }

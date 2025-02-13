a270debc4af5855b673695f4eed787639ed098dd
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2025-02-13 16:42:53 +0300

    15. Аргументы переменной длины (varargs) [#504977]

--- src/main/java/ru/job4j/varargs/VarArgs.java
+++ src/main/java/ru/job4j/varargs/VarArgs.java
@@ -7,14 +7,10 @@ public class VarArgs {
         for (String string : strings) {
             builder.append(string);
         }
-        return builder.toString();
+        return builder + " Количество соединенных элементов: " + strings.length;
     }
 
     public static void main(String[] args) {
-        System.out.println(text());
-        System.out.println(text("aaa"));
         System.out.println(text("aaa", "bbb", "ccc"));
-        System.out.println(text("aaa", "bbb", "ccc", "ddd", "eee"));
     }
-
 }

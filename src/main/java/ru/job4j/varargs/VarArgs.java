1ee34cc089a3c57427c8914ab00c67a7362fc49a
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2025-02-13 16:38:28 +0300

    15. Аргументы переменной длины (varargs) [#504977]

---
+++ src/main/java/ru/job4j/varargs/VarArgs.java
@@ -0,0 +1,17 @@
+package ru.job4j.varargs;
+
+public class VarArgs {
+
+    public String text(String a) {
+        return "";
+    }
+
+    public String text(String a, String b) {
+        return "";
+    }
+
+    public String text(String a, String b, String c) {
+        return "";
+    }
+
+}

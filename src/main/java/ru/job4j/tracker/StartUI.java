0acaf91d4235e2e4602106284f513e76efa1b710
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2025-02-17 22:50:04 +0300

    3.2. Реализация класса StartUI. Вывод всех заявок [#500745]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,13 +1,13 @@
 package ru.job4j.tracker;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class StartUI {
     public void init(Scanner scanner, Tracker tracker) {
         boolean run = true;
         while (run) {
             showMenu();
-
             System.out.print("Выбрать: ");
             int select = Integer.parseInt(scanner.nextLine());
             if (select == 0) {
@@ -17,6 +17,16 @@ public class StartUI {
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Добавленная заявка: " + item);
+            } else if (select == 1) {
+                System.out.println("=== Вывод всех заявок ===");
+                Item[] items = tracker.findAll();
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Хранилище еще не содержит заявок");
+                }
             } else if (select == 6) {
                 run = false;
             }

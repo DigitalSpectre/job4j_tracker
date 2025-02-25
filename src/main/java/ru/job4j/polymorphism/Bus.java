1fdb270e174cbce8e23a0fe1560d191ae8115e59
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2025-02-25 04:32:42 +0300

    5.0. Приведение типов. Повышение типа и понижение типа. [#181322]

--- src/main/java/ru/job4j/polymorphism/Bus.java
+++ src/main/java/ru/job4j/polymorphism/Bus.java
@@ -4,14 +4,17 @@ public class Bus implements Transport {
 
     @Override
     public void move() {
+        System.out.println("Автобус движется.");
     }
 
     @Override
     public void passengers(int count) {
+        System.out.println("Пассажиров в автобусе: " + count);
     }
 
     @Override
     public int refuel(int fuel) {
+        System.out.println("Топлива в баке: " + fuel);
         return 0;
     }
 }

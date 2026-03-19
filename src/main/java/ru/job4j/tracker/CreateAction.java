3e23b62b4d6fca0f7355bf9808668859525d9e91
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2026-03-19 12:40:13 +0300

    11.1. Реализация меню за счет шаблона стратегия. [#181780]

---
+++ src/main/java/ru/job4j/tracker/CreateAction.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+public class CreateAction implements UserAction {
+    @Override
+    public String name() {
+        String[] menu = {
+                "Добавить новую заявку", "Показать все заявки", "Изменить заявку",
+                "Удалить заявку", "Показать заявку по id", "Показать заявки по имени",
+                "Завершить программу"
+        };
+
+        return "Добавить новую заявку";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Создание новой заявки ===");
+        String name = input.askStr("Введите имя: ");
+        Item item = new Item(name);
+        tracker.add(item);
+        System.out.println("Добавленная заявка: " + item);
+        return true;
+    }
+}

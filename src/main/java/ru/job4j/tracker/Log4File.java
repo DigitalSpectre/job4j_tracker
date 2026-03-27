56cb85ef88f995792afc2e5c38e33e165d35cbea
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2026-03-27 16:44:52 +0300

    1. Singleton [#94606]

--- src/main/java/ru/job4j/tracker/Log4File.java
+++ src/main/java/ru/job4j/tracker/Log4File.java
@@ -1,14 +1,17 @@
 package ru.job4j.tracker;
 
-public class Log4File {
+public final class Log4File {
     private String[] messages = new String[1000];
     private int index = 0;
+
+    private Log4File() {
+    }
 
     public void add(String message) {
         messages[index++] = message;
     }
 
     public void save() {
-        /* TODO Сохраняем записи из массива messages в файл. */
+        //TODO Сохраняем записи из массива messages в файл.
     }
 }

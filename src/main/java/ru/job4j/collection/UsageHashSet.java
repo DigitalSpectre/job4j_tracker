80fddaa23e8349bee9eee1bfd995938235569f52
DigitalSpectre <vedroidnytiliponcheg@gmail.com>
2026-05-15 16:38:54 +0300

    0. Set, HashSet, Iterator. [#10095]

---
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UsageHashSet {
+    public static void main(String[] args) {
+        HashSet<String> names = new HashSet<>();
+        names.add("Petr");
+        names.add("Ivan");
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
